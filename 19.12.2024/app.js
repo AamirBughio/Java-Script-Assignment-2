5fb82e331a97923e4d80bf227f348231c5be3f9c
AamirBughio <aamirhashim95@gmail.com>
2024-12-19 20:00:30 +0500

    Add files via upload

---
+++ 19.12.2024/app.js
@@ -0,0 +1,88 @@
+// Chapter No 4
+
+// Legal variable names
+//var myVar;
+//var $price;
+//var _name;
+//var age123;
+//var userName;
+
+//illegal variable names
+//var 123abc
+//var var
+//var my-variable!
+//var @name
+//var user name
+
+// 3. Display this in your browser
+//document.write("<h1>Rules for naming JS variables</h1>");
+//document.write("<p>Variable names can only contain letters, numbers, $, and _. For example: $my_1stVariable</p>");
+//document.write("<p>Variables must begin with a letter, $ or _. For example: $name, _name, or name</p>");
+//document.write("<p>Variable names are case sensitive</p>");
+//document.write("<p>Variable names should not be JS keywords</p>");
+
+
+// Chapter No 5
+
+//Question No 1
+
+//var num1 = 3;
+//var num2 = 5;
+
+//var sum = num1 + num2;
+//document.write("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+//var num1 = 3;
+//var num2 = 5;
+
+//var product = num1 * num2;
+//document.write("The product of " + num1 + " and " + num2 + " is " + product);
+
+//var num1 = 3;
+//var num2 = 5;
+
+//var division = num1 / num2;
+//document.write("The division of " + num1 + " by " + num2 + " is " + division);
+
+//Question No 4
+
+//var ticketPrice = 600;
+//var totalCost = ticketPrice * 5;
+//document.write("The cost of buying 5 tickets to a movie is: " + totalCost + " PKR");
+
+
+//Question No 5
+
+//var number = 4;
+
+//document.write(number + " x 1 = " + (number * 1) + "<br>");
+//document.write(number + " x 2 = " + (number * 2) + "<br>");
+//document.write(number + " x 3 = " + (number * 3) + "<br>");
+//document.write(number + " x 4 = " + (number * 4) + "<br>");
+//document.write(number + " x 5 = " + (number * 5) + "<br>");
+//document.write(number + " x 6 = " + (number * 6) + "<br>");
+//document.write(number + " x 7 = " + (number * 7) + "<br>");
+//document.write(number + " x 8 = " + (number * 8) + "<br>");
+//document.write(number + " x 9 = " + (number * 9) + "<br>");
+//document.write(number + " x 10 = " + (number * 10) + "<br>");
+
+//Chapter No 6
+//Question No 1
+
+//var a = 10;
+//document.write("The value of a is: " + a + "<br><br>");
+
+//document.write("The value of ++a is: " + (++a) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+//document.write("The value of a++ is: " + (a++) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+//document.write("The value of --a is: " + (--a) + "<br>");
+//document.write("Now the value of a is: " + a + "<br><br>");
+
+//document.write("The value of a-- is: " + (a--) + "<br>");
+//document.write("Now the value of a is: " + a + "<br>");
+
+
+    
